aebcb9b13fcc2018e083e7980d2a87128c5cd5a8
Louise Liu <s1881079@baltic05.geos.ed.ac.uk>
2019-04-15 14:08:14 +0100

    testing with sample data

--- workspace_building/codes/mtch_dbs.py
+++ workspace_building/codes/mtch_dbs.py
@@ -10,8 +10,11 @@ import fiona
 #import geopandas as gpd
 import pandas as pd
 import shapely as spl
-import numpy as np
+from shapely import geometry as shg
+#import numpy as np
 import math
+import pyproj
+import os
 
 def readShp(shp_fn):
     '''
@@ -35,14 +38,14 @@ def toSplGeom(l_shp):
     '''
     l_geoms = []
     for shp in l_shp:
-        l_geoms.append(spl.geometry.shape(shp['geometry']))
+        l_geoms.append(shg.shape(shp['geometry']))
         
-    if l_shp[0]['geometry']['type'] == 'Polygon':
-        multi_polygons = spl.geometry.MultiPolygon(l_geoms)
-    else:
-        print('not polygon')
+#    if l_shp[0]['geometry']['type'] == 'Polygon':
+#        multi_polygons = shg.MultiPolygon(l_geoms)
+#    else:
+#        print('not polygon')
         
-    return l_geoms, multi_polygons
+    return l_geoms
     
 
 
@@ -55,10 +58,13 @@ def readGSVInfo(csv_fn):
     '''
     data = pd.read_csv(csv_fn)
     rst = []
+    
+    osgb36 = pyproj.Proj(init = 'epsg:27700')
+    wgs84 = pyproj.Proj(init='epsg:4326')
+    
     for ind,rc in data.iterrows():
         #add pyproj stuff here
-        mapx = 0
-        mapy = 0
+        mapx,mapy = pyproj.transform(wgs84,osgb36,rc.lon,rc.lat)
         
         rst.append({'lat':rc.lat,'lon':rc.lon,'x':mapx,'y':mapy,'fov':rc.fov,'heading':rc.heading,'pitch':rc.url_pitch})
         
@@ -122,35 +128,43 @@ def buildingWithinQuadBuffer(cam_pt,bds_geom,distance):
     rst = []
     ind_rst = []
     #find buildings within the distance range
-    cam_geom = spl.geometry.Point(cam_pt.x,cam_pt.y)
+    cam_geom = shg.Point(cam_pt['x'],cam_pt['y'])
     #quad_split_fun = getQuaSplitFunc(cam_pt.heading)
     cam_round = cam_geom.buffer(distance)
     #here create the lookoing fan directly and close up
     #need to further check the orientation of coordinate system here
-    suqare_coords = [(cam_pt.x,cam_pt.y),(cam_pt.x + distance,cam_pt.y),(cam_pt.x + distance,cam_pt.y + distance),(cam_pt.x,cam_pt.y + distance)]
+    suqare_coords = [(cam_pt['x'],cam_pt['y']),(cam_pt['x'] + distance,cam_pt['y']),(cam_pt['x'] + distance,cam_pt['y'] + distance),(cam_pt['x'],cam_pt['y'] + distance)]
     
-    cam_square = spl.geometry.Polygon(suqare_coords)
+    cam_square = shg.Polygon(suqare_coords)
     
     #skew the square to fit with FOV
-    if cam_pt.fov != 90:
-        cam_square_skew = spl.affinity.skew(cam_square,xs = 90 - cam_pt.fov,origin = (cam_pt.x,cam_pt.y))
+    if cam_pt['fov'] != 90:
+        cam_square_skew = spl.affinity.skew(cam_square,xs = 90 - cam_pt['fov'],origin = (cam_pt['x'],cam_pt['y']))
     else:
         cam_square_skew = cam_square
         
     #rotate the square here
-    cam_square_rot = spl.affinity.rotate(cam_square_skew,cam_pt.heading - 90,origin = (cam_pt.x,cam_pt.y))
+    rot_angle = (-1) * (cam_pt['heading'] - 90 + (cam_pt['fov'] / 2))
+    #print('rot_angle')
+
+    #print(rot_angle)
+    cam_square_rot = spl.affinity.rotate(cam_square_skew,rot_angle,origin = (cam_pt['x'],cam_pt['y']))
     
-    cam_fan = cam_round.interseaction(cam_square_rot)
+    #print('finish quad')
+    #print(cam_square_rot)
+    
+    cam_fan = cam_round.intersection(cam_square_rot)
     
     for bd in bds_geom:
-        if bd.intersection(cam_fan) is not None:
+        if bd.intersects(cam_fan):
             #check if the above format works
-            
+            #print(bds_geom.index(bd))
             rst.append(bd)
             ind_rst.append(bds_geom.index(bd))
     
+    #print('candidate info upper')
     
-    return rst,ind_rst
+    return rst
 
 
 
@@ -165,20 +179,22 @@ def genLineOfSight(cam_pt, distance,ct_ang = 0):
     \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\reasonable to use distance?
     '''
     #check if centre_angle is valid
-    if cam_pt.fov/(-2) < ct_ang < cam_pt.fov/2:
+    if cam_pt['fov']/(-2) < ct_ang < cam_pt['fov']/2:
         pass
     else:
         #invalid center_angle
         return None
-    edpt_x = cam_pt.x + distance * math.sin(math.radians(cam_pt.heading))
-    edpt_y = cam_pt.y + distance * math.cos(math.radians(cam_pt.heading))
-    endpt = spl.geometry.Point(edpt_x,edpt_y)
-    stpt = spl.geometry.Point(cam_pt.x,cam_pt.y)
+    edpt_x = cam_pt['x'] + distance * math.sin(math.radians(cam_pt['heading']))
+    edpt_y = cam_pt['y'] + distance * math.cos(math.radians(cam_pt['heading']))
+    endpt = shg.Point(edpt_x,edpt_y)
+    stpt = shg.Point(cam_pt['x'],cam_pt['y'])
     
     #create new line in geopd
-    sline = spl.geometry.LineString(stpt,endpt)
+    sline = shg.LineString([stpt,endpt])
     
     #calc endpoint and input information
+    #print('sline generated')
+    #print(sline)
     
     return sline
     
@@ -192,29 +208,72 @@ def findFirstHit(sline,building_list):
     #should be a function to calculate the meeting point of two lines - if none, type one
     #find all the meeting points
     l_ints = []
+    lst_hitBD = []
+    #print('length of buildinglist')
+    #print(len(building_list))
     
     for bd in building_list:
+        #print(len(list(bd.bounds.coords)))
         ints = sline.intersection(bd)
-        lst_mpts = list(ints.coords)
+        
+        if ints.length == 0:
+            #print(ints)
+            continue
+        
+        #print('hit bd found')
+        #print(ints)
+        
+        lst_hitBD .append(bd)
+        
+        if ints.geom_type == 'LineString':
+            lst_mpts = list(ints.coords)
+        elif ints.geom_type == 'MultiLineString':
+            lst_mpts = []
+            for ls in ints:
+                lst_mpts += list(ls.coords)
+        
+        #print(lst_mpts)
         
         #find the nearest meeting point within one building
-        lst_mpts.sort(key = lambda x:(abs(x[0] - sline.coords[0][0])))
-        nrest_coord_thisBD = lst_mpts[0]
+        
+        nrest_coord_thisBD = min(lst_mpts, key = lambda x:abs(x[0] - sline.coords[0][0]))
+        #print('nearest coord for this bd:')
+        #print(nrest_coord_thisBD)
         
         l_ints.append(nrest_coord_thisBD)
     
-    l_ints.sort(key = lambda x: (abs(x[0] - sline.coords[0][0])))
-    nearest_coord = l_ints[0]
+    #print('printting information of l_ints')
+    #print(len(l_ints))
+    #print(l_ints)
     
-    nearest_pt = spl.geometry.Point(nearest_coord[0],nearest_coord[1])
     
-    for bd in building_list:
-        if bd.intersection(nearest_pt) is not None:
-            #===========Attention: this is returning the index of another list
-            return building_list.index(bd), nearest_pt
-    else:
-        print('loop throuhg all candidate buildings, target not found ')
-        return None,None
+    try:
+        nearest_coord = min(l_ints, key = lambda x:(abs(x[0] - sline.coords[0][0])))
+        
+        #instead of creating new point, get the oribound point would be better
+        nearest_pt = shg.Point(nearest_coord[0],nearest_coord[1])
+        #print('nearest point found')
+        #print(nearest_pt)
+        
+        #trans to using index to do this
+        #print('nearest point index')
+        #print(l_ints.index(nearest_coord))
+        hitID = l_ints.index(nearest_coord)
+        targetBD = lst_hitBD[hitID]
+        
+        return building_list.index(targetBD),nearest_pt
+    
+    except:
+        print('except error')
+        return 2,shg.Point(1,1)
+    
+#    for bd in building_list:
+#        if bd.touches(nearest_pt):
+#            #===========Attention: this is returning the index of another list
+#            return building_list.index(bd), nearest_pt
+#    else:
+#        print('loop throuhg all candidate buildings, target not found ')
+#        return None,None
         
     
     #return the meeting point and the building id
@@ -235,19 +294,22 @@ def findBuildingInSight(cam_pt,bds_geom,sight_distance):
 
 if __name__ == "__main__":
     #shp_fn = 'SJ89_GM_buildings_districts_270315.shp'
+    #workspace = os.getcwd()
+    os.chdir('../SJ89_GM_buildings_districts_270315')
     shp_fn = 'cliped_OS_building.shp'
-    csv_fn = 'gsv_info.csv'
+    csv_fn = '../gsv_info.csv'
     bds_shp = readShp(shp_fn)
     bds_geom = toSplGeom(bds_shp)
     
     cam_pts = readGSVInfo(csv_fn)
     
-    sight_distance = 200
-    hit_distance = 100
+    sight_distance = 50
     
     for cam_pt in cam_pts:
         target_buildingIndex, look_centre = findBuildingInSight(cam_pt,bds_geom,sight_distance)
         target_buildingID = bds_shp[target_buildingIndex]['properties']['ID']
+        #print('looking at building')
+        #print(bds_shp[target_buildingIndex]['id'])
         cam_pt['t_building'] = target_buildingID
         cam_pt['eye_ct'] = list(look_centre.coords)
         #should find a better way rather than list to do the above process
